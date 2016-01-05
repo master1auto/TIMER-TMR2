f523bbfc3390b7db6c79921cb1e79100e6b28ee8
rahmabelhamra <rahmabelhamra@gmail.com>
2016-01-05 22:06:51 +0100

    Update mini_project.c

--- mini_project.c
+++ mini_project.c
@@ -1,3 +1,19 @@
+void Interrupt(){
+  if (TMR2IF_bit){
+    TMR2IF_bit = 0;
+      portb++;
+  }
+}
+
 void main() {
+  T2CON	         = 0b01001110;
+  PR2		 = 250;
+  TMR2IE_bit	 = 1;
+  INTCON	 = 0b11000000;
+  trisb=0;
+  portb=0;
+  if (portb==255) portb=0;
+  
+
 
 }
